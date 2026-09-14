d57af78ba55e07b4c9f0258c4e39ff331648385e
Patrice-An <antoinepatrice973@gmail>
2026-09-14 11:14:35 +0200

    Init + Hello World

---
+++ src/main/java/com/example/devspring/DevSpringApplication.java
@@ -0,0 +1,14 @@
+package com.example.devspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevSpringApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
